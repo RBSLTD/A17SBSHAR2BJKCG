5fb4573bfe8e0a8e8b72ef08c5e032d9bf958640
mineminemine258 <mineminemine258@gmail.com>
2016-07-19 13:52:29 +0100

    Version 0.0.1.5
    Updated to include login system. This can only be tested by our admins as they know how to manual add users.
    This is until version 0.0.2.0 where the registering system will be incorporated. 
    
    **Note to admins*
    * The user leveling system is currently a work in progress

--- A17SBSHAR2BJKCG/A17SBSHAR2BJKCG/Form1.vb
+++ A17SBSHAR2BJKCG/A17SBSHAR2BJKCG/Form1.vb
@@ -1,3 +1,45 @@
-﻿Public Class SignIn
+﻿Imports System
+Imports System.IO
+Imports System.Text
 
+Public Class SignIn
+    Private Sub SignIn_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim dim1 As String = "C:\Users\Public\RBSLTD"
+        Dim dim2 As String = "C:\Users\Public\RBSLTD\logins"
+        If (Not System.IO.Directory.Exists(dim1)) Then
+            System.IO.Directory.CreateDirectory(dim1)
+            If (Not System.IO.Directory.Exists(dim2)) Then
+                System.IO.Directory.CreateDirectory(dim2)
+            End If
+        End If
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim name As String
+        Dim pass As String
+        Dim pathname As String
+        Dim pathpass As String
+        Dim nameread As String
+        Dim passread As String
+        name = TextBox1.Text
+        pass = TextBox2.Text
+
+        If (Not System.IO.Directory.Exists("C:\Users\Public\RBSLTD\logins\" & name)) Then
+            MsgBox("User does not exist ")
+        Else
+            pathname = "C:\Users\Public\RBSLTD\logins\" & name & "\username.txt"
+            pathpass = "C:\Users\Public\RBSLTD\logins\" & name & "\password.txt"
+            nameread = My.Computer.FileSystem.ReadAllText(pathname)
+            passread = My.Computer.FileSystem.ReadAllText(pathpass)
+            If name = nameread Then
+                If pass = passread Then
+                    MsgBox("Successfully logged in")
+                Else
+                    MsgBox("Invalid username or password")
+                End If
+            Else
+                MsgBox("Invalid username or password")
+            End If
+        End If
+    End Sub
 End Class
